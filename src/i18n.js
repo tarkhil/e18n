34d6c7b98ce9ef2528550ef364e82f5b0d5f4d81
Alex Povolotsky <tarkhil@over.ru>
2021-01-24 23:43:21 +0300

    ElementUI plugin installed

--- src/i18n.js
+++ src/i18n.js
@@ -5,7 +5,7 @@ Vue.use(VueI18n)
 
 function loadLocaleMessages () {
   const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
+  const messages = {  }
   locales.keys().forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
+import ElementUI from 'element-ui'
+import './styles.scss'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
